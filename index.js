97e16aa78e86ff084d9f5ce47e1d7b5c2fadbb43
Bruno César Piovezan Mendonça <bcm_1993@hotmail.com>
2020-11-26 02:46:28 -0300

    atualização

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let target_date = new Date("november 27, 2020").getTime();
+let target_date = new Date("november 26, 2020").getTime();
 let dias, horas, minutos, segundos;
 let regressiva = document.getElementById("regressiva");
 
@@ -16,10 +16,27 @@ setInterval(function () {
     minutos = parseInt(segundos_f / 60);
     segundos = parseInt(segundos_f % 60);
 
-    document.getElementById('dia').innerHTML = "0";
-    document.getElementById('hora').innerHTML = horas - 4;
+    document.getElementById('dia').innerHTML = dias;
+    document.getElementById('hora').innerHTML = horas ;
     document.getElementById('minuto').innerHTML = minutos;
     document.getElementById('segundo').innerHTML = segundos;
+
+    let hora = new Date
+    let dia = hora.getDate()
+    console.log(dia)
+
+    if(dia == 26){
+           
+        target_date = new Date("november 27, 2020").getTime();
+        document.getElementById('dia').innerHTML = 0;
+        document.getElementById('hora').innerHTML = horas -4 ;
+        document.getElementById('minuto').innerHTML = minutos ;
+        document.getElementById('segundo').innerHTML = segundos 
+
+
+    }
+
+
 
 }, 1000);
 
