90ab8f73f56985463061bab8afae0bf0c5bb7400
Bruno César Piovezan Mendonça <bcm_1993@hotmail.com>
2020-11-26 00:46:45 -0300

    atualização

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ setInterval(function () {
     let current_date = new Date().getTime();
     let segundos_f = (target_date - current_date) / 1000;
 
-    dias = parseInt((segundos_f + 71000) / 86400);
-    segundos_f = segundos_f % 8400;
+    dias = parseInt(segundos_f / 86400);
+    segundos_f = segundos_f % 86400 ;
     
     horas = parseInt(segundos_f  / 3600);
     segundos_f = segundos_f % 3600;
@@ -16,8 +16,8 @@ setInterval(function () {
     minutos = parseInt(segundos_f / 60);
     segundos = parseInt(segundos_f % 60);
 
-    document.getElementById('dia').innerHTML = dias;
-    document.getElementById('hora').innerHTML = horas;
+    document.getElementById('dia').innerHTML = "0";
+    document.getElementById('hora').innerHTML = horas - 4;
     document.getElementById('minuto').innerHTML = minutos;
     document.getElementById('segundo').innerHTML = segundos;
 
